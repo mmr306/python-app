b6330764b7620eac0a2e4c4cb9afc1aac030d974
mmr306 <mmr306@gmail.com>
2023-03-18 11:54:13 -0400

    update test

--- test-app/test1.py
+++ test-app/test1.py
@@ -1,1 +1,7 @@
-print('Test1')
+# content of test_sample.py
+def func(x):
+    return x + 1
+
+
+def test_answer():
+    assert func(3) == 5
--- test-app/test3.py
+++ test-app/test3.py
@@ -1,1 +1,7 @@
-print('Test3')
+# content of test_sample.py
+def func(x):
+    return x + 2
+
+
+def test_answer():
+    assert func(3) == 5
