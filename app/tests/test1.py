5647f2f806a360e0be6be6c790149fdc7f8968f5
mmr306 <mmr306@gmail.com>
2023-03-21 13:36:24 -0400

    Update selenium

--- app/tests/test1.py
+++ app/tests/test1.py
@@ -1,17 +1,5 @@
-from selenium import webdriver
-from time import sleep
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.chrome.service import Service as ChromeService
-from webdriver_manager.chrome import ChromeDriverManager
+import requests
 
 def test_lambdatest_todo_app():
-    #chrome_driver = webdriver.Chrome()
-    chrome_driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
-    chrome_driver.get('https://vermontvt.com/')
-
-    title = "Dash"
-    sleep(2)
-    assert title == chrome_driver.title
-    
-    sleep(2)
-    chrome_driver.close()
+    r = requests.get("https://vermontvt.com") 
+    assert r.status_code == 200
