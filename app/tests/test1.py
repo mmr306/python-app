98194ddd63dfc5ad061f6e0e29c90ae3c66f2933
mmr306 <mmr306@gmail.com>
2023-03-21 13:21:50 -0400

    Update chrome webdriver

--- app/tests/test1.py
+++ app/tests/test1.py
@@ -1,9 +1,12 @@
 from selenium import webdriver
 from time import sleep
- 
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+
 def test_lambdatest_todo_app():
-    chrome_driver = webdriver.Chrome()
-    
+    #chrome_driver = webdriver.Chrome()
+    chrome_driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
     chrome_driver.get('https://vermontvt.com/')
 
     title = "Dash"
